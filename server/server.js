b971dbc8701ff20056ba6d1704c3b745bd6015fb
Mathan Ponsingh <mathanponsingh@gmail.com>
2025-12-18 13:55:29 +0530

    server.js updated

--- server/server.js
+++ server/server.js
@@ -43,7 +43,7 @@ await connectDB()
 
 if(process.env.NODE_ENV !=="production"){
     const PORT = process.env.PORT || 5000;
-    server.listen(PORT,()=>{
+    server.listen(PORT ,()=>{
         console.log("Server is running on PORT:"+PORT)
     })
 }
